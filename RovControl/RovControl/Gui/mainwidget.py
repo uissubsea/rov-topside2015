9b119d36d88b8707b763b27d265882db8eafd6ec
Tor Morten Finnesand <tm.finnesand@gmail.com>
2015-03-04 14:32:33 +0100

    In this version, settings and status open in mdi area

--- RovControl/RovControl/Gui/mainwidget.py
+++ RovControl/RovControl/Gui/mainwidget.py
@@ -5,8 +5,9 @@ from Gui import settingswindow
 from Gui import statuswindow
 
 class MainWidget(QtGui.QMainWindow):
+    
     def __init__(self):
-        QtGui.QWidget.__init__(self)
+        super(MainWidget, self).__init__()
 
         # Set up the user interface from Designer.
         self.ui = mainwindow.Ui_MainWindow()
@@ -20,15 +21,22 @@ class MainWidget(QtGui.QMainWindow):
 
         self.show()
 
+        self.subwindow1 = QtGui.QMdiSubWindow()
+        self.subwindow2 = QtGui.QMdiSubWindow()
+
 
     def open_settingsWindow(self):
         self.cfgWindow = QtGui.QWidget()
         self.ui2 = settingswindow.Ui_SettingsWindow()
         self.ui2.setupUi(self.cfgWindow)
+        self.subwindow1.setWidget(self.cfgWindow)
+        self.ui.mdiArea.addSubWindow(self.subwindow1)
         self.cfgWindow.show()
 
     def open_statusWindow(self):
-    	self.statWindow = QtGui.QWidget()
-    	self.ui3 = statuswindow.Ui_StatusWindow()
-    	self.ui3.setupUi(self.statWindow)
-    	self.statWindow.show()
+        self.statWindow = QtGui.QWidget()
+        self.ui3 = statuswindow.Ui_StatusWindow()
+        self.ui3.setupUi(self.statWindow)
+        self.subwindow2.setWidget(self.statWindow)
+        self.ui.mdiArea.addSubWindow(self.subwindow2)
+        self.statWindow.show()
