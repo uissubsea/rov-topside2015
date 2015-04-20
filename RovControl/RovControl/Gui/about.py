dd17ac181087baf31ae5be270af2fc2353471151
Elisabeth B. Baardseth <borg0609@hotmail.com>
2015-04-20 10:32:35 +0200

    Små justeringer i main.py. Opprettet about.py (gui-vinduer for about-menyen)

--- RovControl/RovControl/Gui/about.py
+++ RovControl/RovControl/Gui/about.py
@@ -6,14 +6,45 @@ class UiSSubsea(QtGui.QWidget):
 		super(UiSSubsea, self).__init__()
 		self.initUI()
 
+
 	def initUI(self):
 		# Draw widget window:
-		w = 550
-		h = 450
-		self.setGeometry(100, 100, w, h)
-		self.setMaximumSize (w, h)
-		self.setMinimumSize(w, h)
+		self.w = 550
+		self.h = 450
+		self.center()
+		self.setMaximumSize (self.w, self.h)
+		self.setMinimumSize(self.w, self.h)
 		self.setWindowTitle('UiS Subsea')
+		self.addContents()
+		self.show()
+
+
+	def addContents(self):
+		lbl = QtGui.QLabel('legg til litt fornuftig tekst om uis subsea', self)
+		lbl.setGeometry(30,40,300,300)
+
+
+		self.addButtons()
+
+
+	def addButtons(self):
+		self.okButton = QtGui.QPushButton('OK', self)
+		self.okButton.setGeometry((self.w/2)-45, self.h-50, 90, 30)
+		self.okButton.clicked.connect(self.closeWindow)
+
+
+	def closeWindow(self):
+		self.close()
+
+
+	def center(self):
+		qr = self.frameGeometry()
+		cp = QtGui.QDesktopWidget().availableGeometry().center()
+		qr.moveCenter(cp)
+		self.move(qr.topLeft())
+
+
+
 
 
 class Vehicle(QtGui.QWidget):
@@ -22,11 +53,39 @@ class Vehicle(QtGui.QWidget):
 		super(Vehicle, self).__init__()
 		self.initUI()
 
+
 	def initUI(self):
 		# Draw widget window:
-		w = 550
-		h = 450
-		self.setGeometry(100, 100, w, h)
-		self.setMaximumSize (w, h)
-		self.setMinimumSize(w, h)
-		self.setWindowTitle('This Vehicle')
+		self.w = 550
+		self.h = 200
+		self.center()
+		self.setMaximumSize (self.w, self.h)
+		self.setMinimumSize(self.w, self.h)
+		self.setWindowTitle('Add name of ROV')
+		self.addContents()
+		self.show()
+
+
+	def addContents(self):
+		text = QtGui.QLabel('Add some specs.. :)\n\nName:', self)
+		text.setGeometry(20,40,200,50)
+
+		self.addButtons()
+
+
+	def addButtons(self):
+		self.okButton = QtGui.QPushButton('OK', self)
+		self.okButton.setGeometry((self.w/2)-45, self.h-50, 90, 30)
+		self.okButton.clicked.connect(self.closeWindow)
+
+
+	def closeWindow(self):
+		self.close()
+
+
+	def center(self):
+		qr = self.frameGeometry()
+		cp = QtGui.QDesktopWidget().availableGeometry().center()
+		qr.moveCenter(cp)
+		self.move(qr.topLeft())
+
